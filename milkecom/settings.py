e1a6478abc8ef27acd4e9b262b6c40e2580e5da8
sonalishelke999 <shelkesonali999@gmail.com>
2024-10-05 20:50:48 +0000

    whitenoise

--- milkecom/settings.py
+++ milkecom/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +130,7 @@ USE_TZ = True
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[ os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
